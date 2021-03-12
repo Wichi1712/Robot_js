4fcd519b99cf17721eab5e5f29bc4ed0a7f9b274
Wichi1712 <46914107+Wichi1712@users.noreply.github.com>
2021-03-12 13:11:31 -0500

    Add files via upload

---
+++ robotMision/js/CommandsDefinition.js
@@ -0,0 +1,65 @@
+//Definicion de Comandos
+
+//ünabu -> UNABU - ARRIBA
+//IÑUN - ABAJO
+//OUWEREN / DERECHA
+//BüONA -> BOUNA / IZQUIERDA
+//ACHUBARA -> SALTAR
+
+var principalCommands = [
+	{idP: 'md', id:'md',com: 'MOVERDERECHA'},
+	{idP: 'ma', id:'ma',com: 'MOVERABAJO'},
+	{idP: 'mar', id:'mar',com: 'MOVERARRIBA'},
+	{idP: 'mi', id:'mi',com: 'MOVERIZQUIERDA'},
+
+	{idP: 'sd', id:'sd',com: 'SALTARDERECHA'},
+	{idP: 'sa', id:'sa',com: 'SALTARABAJO'},
+	{idP: 'sar', id:'sar',com: 'SALTARARRIBA'},
+	{idP: 'si', id:'si',com: 'SALTARIZQUIERDA'},
+
+	{idP: 're', id:'re',com: 'REPETIR'},
+	{idP: 'var', id:'var',com: 'VAR'}
+];
+
+var garifunaCommands = [
+
+	{idP:'md', id:'ou', com:'OUWEREN'},
+	{idP:'mi', id:'in', com:'IÑUN'},
+	{idP:'mar', id:'un', com:'UNABU'},
+	{idP:'ma', id:'bo', com:'BOUNA'},
+
+	{idP:'sd', id:'acou', com:'ACHUBARAOUWEREN'},
+	{idP:'si', id:'acin', com:'ACHUBARAIÑUN'},
+	{idP:'sar', id:'acun', com:'ACHUBARAUNABU'},
+	{idP:'sa', id:'acbo', com:'ACHUBARABOUNA'}
+
+];
+
+
+/*var principalCommands = [
+
+{id: 'md', com: 'MOVERDERECHA'}
+{id: 'md', com: 'MOVERABAJO'}
+{id: 'md', com: 'MOVERARRIBA'}
+{id: 'md', com: 'MOVERIZQUIERDA'}
+
+'OUWEREN',
+'IÑUN',
+'UNABU',
+'REPETIR',
+'MIENTRAS',
+'VAR',
+
+'SALTARDERECHA',
+'SALTARIZQUIERDA',
+'SALTARARRIBA',
+'SALTARABAJO'
+'ACHUBARAIÑUN',
+'ACHUBARAUNABU',
+
+
+]; */
+
+
+
+
